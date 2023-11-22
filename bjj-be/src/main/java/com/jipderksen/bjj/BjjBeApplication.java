c6ef4f54ef693f7a2451dbe987d4205675e44ad7
J. Derksen <jip.derksen@hva.nl>
2023-11-22 15:34:17 +0100

    initial commit

---
+++ bjj-be/src/main/java/com/jipderksen/bjj/BjjBeApplication.java
@@ -0,0 +1,13 @@
+package com.jipderksen.bjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjjBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjjBeApplication.class, args);
+	}
+
+}
---
+++ bjj-be/src/test/java/com/jipderksen/bjj/BjjBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jipderksen.bjj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjjBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
