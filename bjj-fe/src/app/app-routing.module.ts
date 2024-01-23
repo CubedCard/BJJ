898ef7b364e86621af1bf788543484707c4f4845
Jip <78497126+CubedCard@users.noreply.github.com>
2024-01-23 17:53:22 +0100

    Created the home page (#9)

--- bjj-fe/src/app/app-routing.module.ts
+++ bjj-fe/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {TrainingsComponent} from './components/trainings/trainings.component';
 import {NewTrainingComponent} from './components/new-training/new-training.component';
+import {HomeComponent} from './components/home/home.component';
 
 const routes: Routes = [
   {path: 'trainings', component: TrainingsComponent},
   {path: 'trainings/new-training', component: NewTrainingComponent},
+  {path: 'home', component: HomeComponent},
   {path: '**', redirectTo: 'trainings'}
 ];
 
