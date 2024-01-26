d0d0ec1b4bba26338b279e5f0eea99e116cf6d2e
Jip <78497126+CubedCard@users.noreply.github.com>
2024-01-26 19:33:29 +0100

    Created all main components and routes (#11)

--- bjj-fe/src/app/app-routing.module.ts
+++ bjj-fe/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import {TrainingsComponent} from './components/trainings/trainings.component';
 import {NewTrainingComponent} from './components/new-training/new-training.component';
 import {HomeComponent} from './components/home/home.component';
 import {LatestTrainingComponent} from './components/latest-training/latest-training.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {SettingsComponent} from './components/settings/settings.component';
 
 const routes: Routes = [
   {path: 'trainings', component: TrainingsComponent},
   {path: 'trainings/new-training', component: NewTrainingComponent},
   {path: 'trainings/latest-training', component: LatestTrainingComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'settings', component: SettingsComponent},
   {path: '**', redirectTo: 'home'}
 ];
 
